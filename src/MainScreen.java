2481342b8ea76b3e9f50d559f12c0d8c3552443c
Mateusz Krasoń <s180450@student.pg.edu.pl>
2021-11-08 23:30:56 +0100

    Added Screen

--- src/MainScreen.java
+++ src/MainScreen.java
@@ -1,2 +1,25 @@
-public class MainScreen {
+import sun.font.TrueTypeFont;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainScreen{
+    private int x = 1024;
+    private int y = 768;
+    public MainScreen(){    }
+    public void showScreen() {
+
+        JFrame screen = new JFrame();
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+        screen.add(panel);
+        screen.setSize(x, y);
+        screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        screen.setTitle("No elo");
+        screen.pack();
+        screen.setVisible(true);
+
+
+    }
 }
+
---
+++ src/main.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+public class main {
+    public static void main(String[] args) {
+        MainScreen screened = new MainScreen();
+        screened.showScreen();
+
+    }
+}
