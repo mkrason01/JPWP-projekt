fa34b20b40857b84356f80e20e791aedc049da4e
Mateusz Krasoń <s180450@student.pg.edu.pl>
2021-11-09 23:23:58 +0100

    Added adding button function

--- src/MainScreen.java
+++ src/MainScreen.java
@@ -9,18 +9,24 @@ public class MainScreen{
 
         JFrame screen = new JFrame();
         JPanel panel = new JPanel();
-        JButton startButton = new JButton("START GRY");
-        panel.setLayout(new BorderLayout());
+        panel.setLayout(new FlowLayout());
         screen.add(panel);
-        panel.add(startButton);
-        startButton.setSize(200,50);
+        panel.add(createButton("START GRY", 400 , 50));
+        panel.add(createButton("siema", 400, 50));
         screen.setSize(x, y);
-        panel.setSize(x, y);
+        screen.setMinimumSize(new Dimension(1024, 768));
+        screen.setMaximumSize(new Dimension(1024, 768));
+        //panel.setSize(x, y);
         screen.setTitle("No elo");
         screen.pack();
         screen.setVisible(true);
 
-
+    }
+    private JButton createButton(String text, int x, int y){
+        JButton b = new JButton(text);
+        b.setSize( x, y );
+        b.setPreferredSize(new Dimension(x,y));
+        return b;
     }
 }
 
