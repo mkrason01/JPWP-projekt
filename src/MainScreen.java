4377b9c6aac172d89cc4e83607fbc12146853b93
Mateusz Krasoń <s180450@student.pg.edu.pl>
2021-12-08 00:44:42 +0100

    Less mess and added createPanel function

--- src/MainScreen.java
+++ src/MainScreen.java
@@ -6,15 +6,17 @@ public class MainScreen{
     private int y = 768;
     public MainScreen(){    }
     public void showScreen() {
-    this.createFrame("Main menu", x, y);
-
-
-
+    JFrame Frame = this.createFrame("Main menu", x, y);
+    JPanel Panel = this.createPanel("Main", x, y);
+    Frame.add(Panel);
+    JButton startGame = this.createButton("Start the game", 300, 100);
+    Panel.add(startGame);
     }
     private JButton createButton(String text, int x, int y){
         JButton b = new JButton(text);
         b.setSize( x, y );
         b.setPreferredSize(new Dimension(x,y));
+        b.setVisible(true);
         return b;
     }
     public JFrame createFrame(String name, int x, int y){
@@ -24,7 +26,14 @@ public class MainScreen{
         frame.pack();
         frame.setVisible(true);
         frame.setSize(x, y);
+        frame.setResizable(false);
         return frame;
+    }
+    public JPanel createPanel(String name, int x, int y){
+        JPanel Panel = new JPanel();
+        Panel.setVisible(true);
+        Panel.setLayout(new FlowLayout());
+        return Panel;
     }
 
 }
