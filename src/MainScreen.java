bfa1c0632eb1ee83adb0cfe0513fba69b328e87c
Mateusz Krasoń <s180450@student.pg.edu.pl>
2021-12-08 00:25:10 +0100

    Less mess and added createFrame function

--- src/MainScreen.java
+++ src/MainScreen.java
@@ -6,20 +6,9 @@ public class MainScreen{
     private int y = 768;
     public MainScreen(){    }
     public void showScreen() {
+    this.createFrame("Main menu", x, y);
 
-        JFrame screen = new JFrame();
-        JPanel panel = new JPanel();
-        panel.setLayout(new FlowLayout());
-        screen.add(panel);
-        panel.add(createButton("START GRY", 400 , 50));
-        panel.add(createButton("siema", 400, 50));
-        screen.setSize(x, y);
-        screen.setMinimumSize(new Dimension(1024, 768));
-        screen.setMaximumSize(new Dimension(1024, 768));
-        //panel.setSize(x, y);
-        screen.setTitle("No elo");
-        screen.pack();
-        screen.setVisible(true);
+
 
     }
     private JButton createButton(String text, int x, int y){
@@ -28,5 +17,15 @@ public class MainScreen{
         b.setPreferredSize(new Dimension(x,y));
         return b;
     }
+    public JFrame createFrame(String name, int x, int y){
+        JFrame frame = new JFrame(name);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setSize(x, y);
+        return frame;
+    }
+
 }
 
